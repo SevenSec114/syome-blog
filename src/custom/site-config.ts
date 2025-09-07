3681e37f4577b608255b1feb82b97cb0843a7e5d
Seven_Sec <1260493648@qq.com>
2025-09-07 15:19:18 +0800

    fix: siteconfig content cannot break line

--- src/custom/site-config.ts
+++ src/custom/site-config.ts
@@ -2,6 +2,7 @@ const siteConfig = {
   siteName: '<your-blog-name>',
   siteDescription: '<your-site-description>',
   author: '<your-author-name>',
+  authorGreet: '<your-author-greet>',
   authorDescription: '<your-author-description>',
   socialLinks: [
     { name: 'GitHub', url: 'https://github.com/SevenSec114' },
